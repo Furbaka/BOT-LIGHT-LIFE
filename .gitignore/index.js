d517b002a60a1992124af2d4354c7c2d75a89f26
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2019-01-03 04:37:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -517,7 +517,7 @@ if (message.attachments.size > 0) {
         message.react(bot.emojis.get("530170778663125022"));
 	message.react(bot.emojis.get("530170738599133204"));
     }
-if (message.reactions.count > (bot.guilds.memberCount / 2 ) + 1 - bot.guilds.memberCount.bot){ 
+if (message.reactions.count > (bot.guilds.memberCount / 2 ) + 1 - bot.guilds.members.filter(member => !member.user.bot).size){ 
 	message.pin(message.id);
 }
 });
