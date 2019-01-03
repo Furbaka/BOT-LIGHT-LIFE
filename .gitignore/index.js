426928b2ddf25ba3b4a3315290b7b705b6b84cd9
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2019-01-03 17:50:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -526,7 +526,7 @@ const filter = (reaction, user) => reaction.emoji.id === '530170738599133204' &&
 
   .then(collected => console.log(`Collected ${collected.size} reactions`))
   .catch(console.error);
-  if(message.awaitReactions.collected > guild.members.filter(member => !member.user.bot).size / 3) { 
+  if(collected.size > bot.guild.memberCount / 3) { 
 message.pin(message.id);
 
 
